3093498aae6f4270f771367815fb91ce796212a6
yueda1984 <61306261+yueda1984@users.noreply.github.com>
2024-04-23 16:38:58 +0900

    Install instruction update

--- src/ANM_Batch_Rename_Cels.js
+++ src/ANM_Batch_Rename_Cels.js
@@ -16,8 +16,8 @@
 	2) Locate to your user scripts folder (a hidden folder):
 	   https://docs.toonboom.com/help/harmony-17/premium/scripting/import-script.html
 	   
-	3) Add all unzipped files (*.js, *.ui, and script-icons folder) directly to the folder above.	
-	4) Add ANM_Batch_Rename_Cels to any toolbar.
+	3) There is a folder named "src" inside the zip file. Copy all its contents directly to the folder above.
+	4) In Harmony, add ANM_Batch_Rename_Cels on any toolbar.
 
 	
 	Direction:
